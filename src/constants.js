abd3a84676f1a01fe76591b88c47606d7b6c209b
Nitish Shankar <nitish3542@gmail.com>
2024-12-12 19:26:08 +0530

    mernFront

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 
 const API_URL = process.env.NODE_ENV != 'development' ?
     process.env.REACT_APP_BASE_URL :
-    'https://backendmern-ehve.onrender.com/'
+    'https://mernback-fjmy.onrender.com'
 
 console.log(process.env, "API_URL");
 
